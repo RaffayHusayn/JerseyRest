d643695ca8a994dfbfd606a76626ffe2c1cbf29b
Raffay Syed <raffayhusayn@gmail.com>
2021-11-12 21:32:19 -0500

    Returns a list of Weathers

--- src/main/java/com/fclass/WeatherResource.java
+++ src/main/java/com/fclass/WeatherResource.java
@@ -4,18 +4,23 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
+import java.util.Arrays;
+import java.util.List;
 
 @Path("weather")
 public class WeatherResource {
 
    @Produces(MediaType.APPLICATION_JSON)
    @GET
-    public Weather getWeather(){
+    public List<Weather> getWeather(){
 
         Weather ny = new Weather();
         ny.setTemp(44);
         ny.setPrecipitation("high");
-        return ny;
+        Weather ca = new Weather();
+        ca.setPrecipitation("low");
+        ca.setTemp(70);
+        List<Weather> weathers = Arrays.asList(ca, ny);
+        return weathers;
     }
 }
